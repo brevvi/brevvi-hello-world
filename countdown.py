0d864a43713166d1c496cb6d733f520aa335fcfc
brevvi <brevvi@gmail.com>
2021-03-21 18:24:31 -0300

    shutdown corrigido

--- countdown.py
+++ countdown.py
@@ -1,15 +1,17 @@
-def countdown(n):
-    if n == 0:
-        print('Shutdown')
-    elif n < 0
-        print('Please insert a numember greater than zero.')
-    else:
-        for i in range(len(n)):
-            if i > 0:
-                print(i)
-                n = i - 1
-            else:
-                print('Shutdown')
+import time
 
-n = input()
+def countdown(n):
+    for i in range(0,n):
+        if n == 0:
+            print('Shutdown!')
+        elif n < 0:
+            print('Please insert a numember greater than zero.')
+        else:
+            time.sleep(1)
+            print(n-1)
+        n = n - 1
+    print('System shutdown!')
+
+print('Contagem regressiva:')
+n = int(input())
 countdown(n)
