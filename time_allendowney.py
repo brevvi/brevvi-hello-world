6607334209c5d69c1fbfbc4d21fbf9a7279c7314
Ricardo de Avila Amaral <brevvi@gmail.com>
2021-03-23 22:05:19 -0300

    Update time_allendowney.py

--- time_allendowney.py
+++ time_allendowney.py
@@ -15,48 +15,48 @@ class Time:
     attributes: hour,  minute, second
     """
     
-    def print_time(t):
-        """ Print a string representation of the time
-        t: Time object
-        """
-        print('%.2d:%.2d:%.2d'%(t.hour, t.minute, t.second))
+def print_time(t):
+    """ Print a string representation of the time
+    t: Time object
+    """
+    print('%.2d:%.2d:%.2d'%(t.hour, t.minute, t.second))
         
-    def int_to_time(seconds):
-        """ Makes a new Time object
-        seconds: int seconds since midnight
-        """
-        time = Time()
-        minutes, time.second = divmod(seconds ,60)
-        time.hour, time.minute = divmod(minutes, 60)
-        return time
+def int_to_time(seconds):
+    """ Makes a new Time object
+    seconds: int seconds since midnight
+    """
+    time = Time()
+    minutes, time.second = divmod(seconds ,60)
+    time.hour, time.minute = divmod(minutes, 60)
+    return time
     
-    def time_to_int(time):
-        """ Computes the number of seconds since midnight
-        time: Time object
-        """
-        minutes = time.hour * 60 + time.minute
-        seconds = minutes * 60 + time.second
-        return seconds
+def time_to_int(time):
+    """ Computes the number of seconds since midnight
+    time: Time object
+    """
+    minutes = time.hour * 60 + time.minute
+    seconds = minutes * 60 + time.second
+    return seconds
     
-    def add_time(t1, t2):
-      """ Adds two time objects
-      t1, t2: Time
-      returns: Time
-      """
-      assert valid_time(t1) and valid_time(t2) # assert valid if it's True
-      seconds = time_to_int(t1) + time_to_int(t2)
-      return int_to_time(seconds)
+def add_time(t1, t2):
+    """ Adds two time objects
+    t1, t2: Time
+    returns: Time
+    """
+    assert valid_time(t1) and valid_time(t2) # assert valid if it's True
+    seconds = time_to_int(t1) + time_to_int(t2)
+    return int_to_time(seconds)
     
-    def valid_time(time):
-      """ Checks wheter a Time object satisfies the invariants
-      time: Time
-      returns: boolean
-      """
-      if time.hour < 0 or time.minute < 0 or time.second < 0:
-          return False
-      if time.minute >= 60 or time.second >= 60:
-          return False
-      return True
+def valid_time(time):
+    """ Checks wheter a Time object satisfies the invariants
+    time: Time
+    returns: boolean
+    """
+    if time.hour < 0 or time.minute < 0 or time.second < 0:
+        return False
+    if time.minute >= 60 or time.second >= 60:
+        return False
+    return True
     
 def main():
     # if a movie start at noon...
