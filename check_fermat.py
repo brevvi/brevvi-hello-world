b1241a0f5a7d69d8ec59d8010ab715f47ae067c0
Ricardo de Avila Amaral <brevvi@gmail.com>
2021-03-23 23:04:02 -0300

    Create check_fermat.py

---
+++ check_fermat.py
@@ -0,0 +1,30 @@
+# ultimo teorema de Fermat
+# b**n + c**n = a**n
+# if 'n' is a prime number and 'a' is a positive integer less then 'n'
+# then a**n modulo n = a modulo 'n'.
+# n = 2
+# a = 1
+# 1**2 % 2 == 1
+# n < 3, a E [1,2]
+# 1**3 mod3 = 1
+#
+# 1**2 % 2 == 1
+# 1**3 % 3 == 1
+# 2**3 % 3 == 2
+# 1**5 & 5 == 1
+# 2**5 & 5 == 2
+#
+# given a number 'n', we want to test if it is prime
+#
+# 1. Pick a roundom number 'a' E [1, n-1]
+# 2. Check if a**n % n == a
+# 3. if yes return true
+# 4. if no, return false
+import random
+random.seed()
+rando.randint(1,10)
+
+is_fermat_prime(2,10)
+find_fermat_prime([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10)
+
+
