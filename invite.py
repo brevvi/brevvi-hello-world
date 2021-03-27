60ea1e2057f3edfb40a8cdf0ec780d459f77cee1
brevvi <brevvi@gmail.com>
2021-03-27 14:02:17 -0300

    inviting some girls to dinner with me

--- invite.py
+++ invite.py
@@ -4,18 +4,39 @@
 """
 import time
 
-girls = ['Jessica Alba','Alexandra Daddario', 'Mary Elizabeth Winstead']
+#girls = ['Jessica Alba','Alexandra Daddario', 'Mary Elizabeth Winstead']
 numb = 0
 
+def checklist():
+    global  girls
+    girls = ['Jessica Alba','Alexandra Daddario', 'Mary Elizabeth Winstead']
+    grlck = girls
+    newname = ' '
+    numb = 0
+    for i in range(len(grlck)):
+        grlcklst = grlck.pop(i)
+        print(grlcklst)
+        time.sleep(1)
+        print(grlcklst+':'+'RSVP')
+        ckconf = str(input(grlcklst+' : '+'Confirm presence? '+'Yes or No? '))
+        ckconf = ckconf.title()
+        if (ckconf == 'Yes' or ckconf == 'Y'):
+            print('Presence  confirmed!')
+            grlck.insert(numb, grlcklst)
+        else:
+            newname = str(input('You do like to suggest a new name:' + '\nNew name: '))
+            grlck.insert(numb, newname)
+        numb = numb + 1
+    girls = grlck.copy()
+    return girls
+
 def invite():
-    #print(girls)
+    numb = 0
     for i in range(len(girls)):
-        #print(girls)
-        girl    =  girls.pop(numb)
+        girl = girls.pop(numb)
         print('Hi,'+girl+'!''\nDo you like to have a dinner with me?')
         answer = str(input('Yes or No?'))
         answer = answer.title()
-        #print(answer)
         if (answer == 'Yes' or answer == 'Y'):
             print("Great! I'll get you at 7pm! XOXO!")
             time.sleep(1)
@@ -25,4 +46,5 @@ def invite():
             time.sleep(1)
     return
 
+checklist()
 invite()
